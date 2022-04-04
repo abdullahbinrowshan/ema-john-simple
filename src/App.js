a4529ede74def570bf291b0caab02f20df22c757
Abdullah Bin Rowshan <abdullahbinrowshan@gmail.com>
2022-04-04 11:27:17 +0600

    Routes added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import { Route, Routes } from 'react-router-dom';
+import Order from './components/Orders/Order';
+import Inventory from './components/Inventory/Inventory';
 
 function App() {
   return (
     <div>
-     <Header></Header>
-     <Shop></Shop>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Shop />}></Route>
+        <Route path='/shop' element={<Shop />}></Route>
+        <Route path='/orders' element={<Order />}></Route>
+        <Route path='/inventory' element={<Inventory />}></Route>
+      </Routes>
     </div>
   );
 }
